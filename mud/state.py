e4c99e8060dc73f5d7b404a94cc20bfb5f4e26de
David Reichard <david@david-Aspire-5920G.(none)>
2011-06-24 21:01:47 -0400

    Minor changes.

--- mud/state.py
+++ mud/state.py
@@ -1,5 +1,14 @@
 class State(object):
     name = "Default State"
     
-    def on_readline(line):
+    def __init__(self, connection):
+        self.connection = connection
+        
+    def on_gainfocus(self):
+        pass
+    def on_losefocus(self):
+        pass
+    def on_readline(self, line):
+        pass
+    def on_write(self):
         pass
